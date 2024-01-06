228180a9eecd716f352aab60087f3bd76d2af7ae
mizuki-boop <149584819+mizuki-boop@users.noreply.github.com>
2024-01-06 20:07:00 +0900

    Add files via upload

---
+++ score.php
@@ -0,0 +1,15 @@
+<?php
+$student_1=80;
+$student_2=60;
+$student_3=55;
+$student_4=40;
+$student_5=100;
+$student_6=25;
+$student_7=80;
+$student_8=95;
+$student_9=30;
+$student_10=60;
+
+echo ($student_1+$student_2+$student_3+$student_4+$student_5+
+$student_6+$student_7+$student_8+$student_9+$student_10)/10;
+?>
