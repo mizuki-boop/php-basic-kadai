7469b49865ff4b7ed6df48d7590089f88300e9bc
mizuki-boop <149584819+mizuki-boop@users.noreply.github.com>
2024-01-12 22:52:51 +0900

    Add files via upload

--- sort.php
+++ sort.php
@@ -8,21 +8,26 @@
 
 <body>
     <p>
-        <?php
+    <?php
+
+    $nums = [15, 4, 18, 23, 10];
+
+    function sort_2way($array,$order){    
+        if($order===true){
+            echo '昇順にソートします。<br>';
+            sort($array);   
+        }else{
+            echo '降順にソートします。<br>';
+            rsort($array);
+        }
+        foreach ($array as $arra) {
+            echo $arra.'<br>';
+        };
     
-       $nums = array(15,4,18,23,10);
-      
-echo '昇順にソートします。<br>';
-sort($nums);
-foreach ($nums as $key => $val) {
-    echo $val.'<br>';
-}
-echo '降順にソートします。<br>';
-rsort($nums);
-foreach ($nums as $key => $val) {
-    echo $val.'<br>';
-}
-        ?>
+    };
+    sort_2way($nums,true);
+    sort_2way($nums,false);       
+    ?>
     </p>
 </body>
 
